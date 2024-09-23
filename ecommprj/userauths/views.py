dd644929db2abbb96a403d07e29a2b6f9c99a01d
TahirMurtaza <tahirmurtaza5152@gmail.com>
2024-09-24 01:45:44 +0500

    flash messages and alerts

--- ecommprj/userauths/views.py
+++ ecommprj/userauths/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from userauths.models import User
 
 # Create your views here.
 def register_view(request):
@@ -19,3 +20,34 @@ def register_view(request):
             return redirect("core:index")
     context = {'form': form}
     return render(request, "userauths/sign-up.html",context)  
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.warning("You already logged in.")
+        return redirect('core:index')
+    
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(request, email=email,password=password)
+            if user:
+                login(request,user)
+                messages.success(request,"You are logged in.")
+            else:
+                messages.warning(request,"You are not registered. Please create new account.")
+            return redirect('core:index')
+        except:
+            messages.warning(request,f"User with {email} doesn't exist.")
+            
+        
+    context = {}
+    return render(request, "userauths/sign-in.html",context)
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have logged out.")       
+    return redirect('userauths:sign-in')
+
